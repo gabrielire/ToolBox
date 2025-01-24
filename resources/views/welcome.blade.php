b5a8f4c723df3d1fa1d3cb3a455c5154c0667928
Gabriel Ire <gabrielirefc21@gmail.com>
2025-01-24 08:08:56 +0700

    genakno gambare nang samping kanan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,6 +7,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-black text-white">
+    <!-- Navbar -->
     <nav class="bg-yellow-500 flex justify-between items-center p-4">
         <div class="flex space-x-4 gap-5 ml-56">
             <a href="#" class="bg-black text-white px-4 py-2 font-bold">Beranda</a>
@@ -17,11 +18,13 @@
     </nav>
     
     <section class="relative text-center py-20 bg-[url('{{ asset('img/t.png') }}')] bg-no-repeat bg-center bg-cover h-screen">
-        <div class="absolute inset-0 bg-black opacity-70"></div>
-        <div class="relative z-10 max-w-4xl mx-auto">
+        <div class="w-1/2">
             <h1 class="text-6xl font-bold">Tool<span class="text-yellow-500">box.id</span></h1>
             <p class="text-lg mt-4">Pusat Perkakas Terlengkap</p>
             <button class="mt-6 bg-yellow-500 text-black font-bold py-3 px-6 rounded-lg">Lihat Sekarang</button>
+        </div>
+        <div class="w-1/2 flex justify-end">
+            <img src="{{ asset('img/a.png') }}" class="max-w-md">
         </div>
     </section>
 </body>
